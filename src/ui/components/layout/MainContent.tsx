680a006fb53cd9dc2f1c7a550635aeffe618ef37
Catflare <nekoflare@users.noreply.github.com>
2026-01-07 18:12:36 +0100

    feat: more gradients

--- src/ui/components/layout/MainContent.tsx
+++ src/ui/components/layout/MainContent.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { NormalText } from '@/ui/components/common/NormalText';
+import { cn } from '@/utils/cn';
 
 /**
  * @description Main chat area content component.
  */
 export const MainContent: React.FC = () => {
     return (
-        <main className="flex-1 flex flex-col items-center justify-center relative z-10">
+        <main
+            className={cn(
+                'flex-1 flex flex-col items-center justify-center relative z-10',
+                'bg-linear-to-r from-bg-secondary from-0% to-[--color-background] to-10%'
+            )}
+        >
             <NormalText>nothing here yet but us servals~!</NormalText>
         </main>
     );
