748bb736698a8e889d8341306b966653ec330a28
Catflare <nekoflare@users.noreply.github.com>
2026-03-05 18:58:56 +0100

    well um actually wrong folder and wahhh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,2 @@
-# to anyone reading this: this shit will deploy to server named "sc0" thats my serchat main prod server. so change the "sc0" here for your liking
-# also yes.. it goes through backend server because no static.catfla.re server :p
-
-scp ../releases.tar.xz sc0:/root/Rolling/backend/
+scp ../releases.tar.xz sc0:/root/
 ssh sc0 "~/deploy-all.sh"
